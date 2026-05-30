559d06248b96eb146c6a20c3969d7c599143478c
Devendra chawda <devendrachawda0773@gmail.com>
2026-05-30 22:26:15 +0530

    added expense tracker project

---
+++ expense_tracker.py
@@ -0,0 +1,47 @@
+while True:
+
+    print("\n--- Expense Tracker ---")
+
+    print("1. Add Expense")
+
+    print("2. View Expenses")
+
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+
+        item = input("Enter expense name: ")
+
+        amount = input("Enter amount: ")
+
+        file = open("expenses.txt", "a")
+
+        file.write(item + " - " + amount + "\n")
+
+        file.close()
+
+        print("Expense added successfully!")
+
+    elif choice == "2":
+
+        file = open("expenses.txt", "r")
+
+        data = file.read()
+
+        print("\nExpenses:\n")
+
+        print(data)
+
+        file.close()
+
+    elif choice == "3":
+
+        print("Exiting program...")
+
+        break
+
+    else:
+
+        print("Invalid choice")
